dbeb15dcf9bf5fbd946a47f76d970715e858ee6a
Silvan Ferreira <sjrmiranda@gmail.com>
2023-05-27 22:22:44 -0300

    first commit

---
+++ setup.py
@@ -0,0 +1,34 @@
+from setuptools import setup, find_packages
+
+with open('README.md', 'r') as f:
+    long_description = f.read()
+
+setup(
+    name='torchninja',
+    version='0.1.3',
+    author='Silvan Ferreira',
+    author_email='silvanfj@gmail.com',
+    description='Tools for training PyTorch models',
+    long_description=long_description,
+    long_description_content_type='text/markdown',
+    url='https://github.com/silvaan/torchninja',
+    packages=find_packages(),
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+    install_requires=[
+        'matplotlib',
+        'numpy',
+        'torch',
+        'torchvision',
+        'tensorboard',
+        'tqdm',
+    ],
+)
